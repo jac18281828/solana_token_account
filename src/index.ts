d1b3c51986025912995d41cfb232627a2d5b23ec
John Cairns <john@2ad.com>
2022-02-05 13:54:00 -0600

    Pull token metadata for accounts on solana

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import * as dotenv from 'dotenv'
 import { Solana } from './solana'
-import { PublicKey, PublicKeyInitData } from '@solana/web3.js'
 
 dotenv.config();
 
 (async () => {
   const netName: string | undefined = process.env.NETWORK
 
-  if(!netName) {
+  if (!netName) {
     throw new Error('please visit .env for environment configuration')
   }
 
   const sol = new Solana(netName ? netName : 'devnet')
 
   sol.connect()
-  
+
   sol.queryTokenAccount()
-  
 })()
--- src/solana.ts
+++ src/solana.ts
@@ -1,9 +1,8 @@
 import { Connection, PublicKey, Cluster, clusterApiUrl } from '@solana/web3.js'
 import { Metadata } from '@metaplex-foundation/mpl-token-metadata'
+import { metadata } from '@metaplex/js/lib/utils'
 
-export const TOKEN_PROGRAM_ID: PublicKey = new PublicKey(
-  'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
-)
+export const TOKEN_PROGRAM_ID: PublicKey = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
 
 export class Solana {
   network: string
@@ -21,44 +20,56 @@ export class Solana {
   /// @throws (Error)
   async queryTokenAccount() {
     if (this.connection) {
-      const prefix = 'aa'
-      const accountList = await this.connection.getProgramAccounts(
-        TOKEN_PROGRAM_ID,
-        {
-          encoding: 'base64',
-          dataSlice: {
-            length: 0,
-            offset: 0,
+      const walletPrefix = 'aa'
+      const accountList = await this.connection.getProgramAccounts(TOKEN_PROGRAM_ID, {
+        encoding: 'base64',
+        dataSlice: {
+          length: 0,
+          offset: 0,
+        },
+        filters: [
+          {
+            dataSize: 165,
           },
-          filters: [
-            {
-              dataSize: 165,
+          {
+            memcmp: {
+              offset: 32,
+              bytes: walletPrefix,
             },
-            {
-              memcmp: {
-                offset: 32,
-                bytes: prefix,
-              },
-            },
-          ],
-        }
-      )
-
-      console.log(`${accountList.length} accounts found for ${prefix}`)
-      accountList.forEach(async (account) => {
-        console.log(account.pubkey.toBase58())
-        /*
-        const mintKey = account.account.data["parsed"]["info"]["mint"];
-        console.log(`token mint: ${mintKey}`);
-        try {
-          const metadataPDA = await Metadata.getPDA(new PublicKey(mintKey));
-          const tokenMeta = await Metadata.load(this.connection, metadataPDA);
-          console.log(tokenMeta);
-        } catch (error) {
-          console.log(`unexpected error ${mintKey}: ${error}`);
-        }
-        */
+          },
+        ],
       })
+
+      console.log(`${accountList.length} accounts found for ${walletPrefix}`)
+      for (let i = 0; i < accountList.length; i++) {
+        const account = accountList[i]
+        console.log(`account address: ${account.pubkey.toBase58()}`)
+        const accountInfo = await this.connection.getParsedAccountInfo(account.pubkey)
+        const infoStruct = accountInfo.value?.data['parsed']['info']
+        const mintAddress = infoStruct['mint']
+        const tokenAmount = infoStruct.tokenAmount
+        // check for an nft decimals 0 and only one token
+        if (tokenAmount.decimals === 0 && parseInt(tokenAmount.amount) === 1) {
+          console.log(`nft mint: ${mintAddress}`)
+          try {
+            const metadataPDA = await Metadata.getPDA(new PublicKey(mintAddress))
+            const tokenMeta = await Metadata.load(this.connection, metadataPDA)
+            const metadataData = tokenMeta.data
+            console.log(`must be verified: ${metadataData.collection?.verified}`)
+            console.log(`must not be mutable: ${!metadataData.isMutable}`)
+            const metadatadatadata = metadataData.data
+            console.log(`name: ${metadatadatadata.name}`)
+            console.log(`creators: ${metadatadatadata.creators}`)
+            console.log(`external uri: ${metadatadatadata.uri}`)
+            console.log(`symbol: ${metadatadatadata.symbol}`)
+            console.log(`seller fee bps: ${metadatadatadata.sellerFeeBasisPoints}`)
+          } catch (error) {
+            console.log(`unexpected error ${mintAddress}: ${error}`)
+          }
+        } else {
+          console.log(`token mint ${mintAddress} does not store an nft`)
+        }
+      }
     } else {
       throw new Error('Not connected')
     }
